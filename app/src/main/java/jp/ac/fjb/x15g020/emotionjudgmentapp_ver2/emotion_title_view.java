b89e82c5ac64a9cab1d8dac6b4bf8d72978a65b7
green-kouhei <kouhei_shoji@green-minto.com>
2017-10-05 11:52:36 +0900

    first commit

---
+++ app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp_ver2/emotion_title_view.java
@@ -0,0 +1,13 @@
+package jp.ac.fjb.x15g020.emotionjudgmentapp_ver2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class emotion_title_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.emotion_title);
+    }
+}
