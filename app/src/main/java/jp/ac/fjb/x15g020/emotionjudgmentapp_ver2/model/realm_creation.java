87040bbb05eb68f915d8397045351b4e90b2b1d5
green-kouhei <kouhei_shoji@green-minto.com>
2017-10-05 14:47:23 +0900

    add java file 環境元どおりにした、まだ動いてる！ 一応もとどおり？

---
+++ app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp_ver2/model/realm_creation.java
@@ -0,0 +1,8 @@
+package jp.ac.fjb.x15g020.emotionjudgmentapp_ver2.model;
+
+/**
+ * Created by Kouhei on 2017/10/05.
+ */
+
+public class realm_creation {
+}
---
+++ app/src/main/java/jp/ac/fjb/x15g020/emotionjudgmentapp_ver2/presentation/select_stage.java
@@ -0,0 +1,8 @@
+package jp.ac.fjb.x15g020.emotionjudgmentapp_ver2.presentation;
+
+/**
+ * Created by Kouhei on 2017/10/05.
+ */
+
+public class select_stage {
+}
